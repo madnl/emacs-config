cc71091421500a7970ef03f2c152615f1f18f4ad
Marius-Andrei Danila <mdanila@twitter.com>
2015-04-18 22:29:15 +0100

    config changes

--- init.el
+++ init.el
@@ -23,14 +23,30 @@
     exec-path-from-shell
     smex
     projectile
+    expand-region
+    auto-complete
+    multiple-cursors
+    ido-ubiquitous
+    idle-highlight-mode
+    find-file-in-project
     
     ;; Clojure
     clojure-mode
     clojure-mode-extra-font-locking
     cider
+    clojure-cheatsheet
+
+    ;; Scala
+    scala-mode2
+
+    ;; Rust
+    rust-mode
 
     ;; HTML
     tagedit
+
+    ;; Markdown
+    markdown-mode
     
     ;; Themes
     solarized-theme
@@ -51,17 +67,39 @@
 
 (require 'ido)
 (ido-mode t)
+(setq ido-enable-flex-matching t)
 
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 
+;; uniquify
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+;; VC
+
+; disabling VC since it is often too slow to load
+(setq vc-handled-backends ())
+
+;; Auto-complete
+(setq ac-ignore-case nil)
+
+(global-auto-complete-mode t)
+
 ;; Editing
 
 (electric-pair-mode t)
 (setq-default indent-tabs-mode nil)
+(define-key global-map (kbd "RET") 'newline-and-indent)
 
 (tool-bar-mode -1)
+
+(setq x-select-enable-clipboard t
+      x-select-enable-primary t
+      visible-bell t)
+
 
 ;; Keyboard shortcuts
 
@@ -75,43 +113,48 @@
 (global-set-key (kbd "s-<up>") 'scroll-down-command)
 (global-set-key (kbd "s-<down>") 'scroll-up-command)
 
+(global-set-key (kbd "s-{") 'previous-buffer)
+(global-set-key (kbd "s-}") 'next-buffer)
+
+(require 'expand-region)
+(global-set-key (kbd "s-=") 'er/expand-region)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "s-D") 'mc/edit-lines)
+(global-set-key (kbd "s-d") 'mc/mark-next-like-this)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "s-p") 'find-file-in-project)
+
+;; Hooks
+
+(add-hook 'clojure-mode-hook #'paredit-mode)
+
 ;; Backup stuff
 
 (setq backup-directory-alist `(("." . "~/.emacs.backup")))
+
+;; Multi-term
+
+(setq multi-term-program "/opt/twitter/bin/zsh")
+
+(let ((theme (if (display-graphic-p)
+                 'solarized-dark
+               'wombat)))
+  (load-theme theme t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(fci-rule-color "#073642")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors (--map (solarized-color-blend it "#002b36" 0.25) (quote ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
- '(hl-bg-colors (quote ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors (quote ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(magit-diff-use-overlays nil)
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(tool-bar-mode nil)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map (quote ((20 . "#dc322f") (40 . "#c85d17") (60 . "#be730b") (80 . "#b58900") (100 . "#a58e00") (120 . "#9d9100") (140 . "#959300") (160 . "#8d9600") (180 . "#859900") (200 . "#669b32") (220 . "#579d4c") (240 . "#489e65") (260 . "#399f7e") (280 . "#2aa198") (300 . "#2898af") (320 . "#2793ba") (340 . "#268fc6") (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list (quote (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+ '(tool-bar-mode nil))
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
